40f56c7cb380a253faaedf0a6e2d2996066a67fb
GilLeibo <59572851+GilLeibo@users.noreply.github.com>
2023-07-01 21:12:30 +0300

    Add files via upload

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,26 +1,26 @@
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
 import torchvision
+from sklearn.metrics.pairwise import cosine_similarity
 
 if __name__ == '__main__':
     # set input file
     file_name = "egg1"
 
-    file_path = file_name+'.xlsx'
+    result_excel_path = "/home/gilnetanel/Desktop/results/" + file_name + ".xlsx"
 
     # Set the time in the video (in seconds) from which the food considered as burned
     time_burned = 110
 
-    # load video and get frames
-    input_file_path = file_name + ".mp4"
+    # load video and get the index of first burned_frame
+    input_file_path = "/home/gilnetanel/Desktop/input/" + file_name + ".mp4"
     torchvision.set_video_backend("pyav")
     video_path = input_file_path
     video = torchvision.io.VideoReader(video_path, "video")
-    video.seek(time_burned)
-    frame = next(video)
+    video_fps = (video.get_metadata().get('video')).get('fps')[0]
+    time_burned_frame_index = int(time_burned * video_fps)
 
     # Read the XLSX file into a pandas DataFrame
-    df = pd.read_excel(file_path, sheet_name='Sheet1')
+    df = pd.read_excel(result_excel_path, sheet_name='Sheet1')
 
     # Convert the DataFrame to a numpy array and transpose it
     numpy_array = (df.to_numpy()).transpose()
@@ -28,3 +28,6 @@ if __name__ == '__main__':
     # Calculate cosine similarity between embedding vectors
     distances = cosine_similarity(numpy_array)
 
+    # calc the threshold which frames with cosine_similarity (compare to the first frame)
+    # lower than that will be considered burned
+    threshold = distances[time_burned_frame_index][0]
