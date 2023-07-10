9e2c2b2d953a8709ba093e045592c5558fa214c2
GilLeibo <59572851+GilLeibo@users.noreply.github.com>
2023-07-10 23:36:55 +0300

    Add files via upload

--- MLP_training.py
+++ MLP_training.py
@@ -12,17 +12,53 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(768, 384),
+            nn.Linear(input_size, input_size/2),
             nn.ReLU(),
-            nn.Linear(384, 192),
+            nn.Linear(input_size/2, input_size/4),
             nn.ReLU(),
-            nn.Linear(192, 768),
+            nn.Linear(input_size/4, input_size),
         )
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
+
+
+def train_one_epoch(epoch_index, tb_writer):
+    running_loss = 0.
+    last_loss = 0.
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(training_loader):
+        # Every data instance is an input + label pair
+        inputs, labels = data
+
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        outputs = model(inputs)
+
+        # Compute the loss and its gradients
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000 # loss per batch
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            tb_x = epoch_index * len(training_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            running_loss = 0.
+
+    return last_loss
 
 
 def get_values_according2_embedding_format(df, embedding_format):
@@ -57,6 +93,8 @@ embedding_formats_dict = {
     "7": "hsv"
 }
 
+# 768 is the default, embeddings_only size
+input_size = 768
 
 if __name__ == "__main__":
 
@@ -64,13 +102,73 @@ if __name__ == "__main__":
     embedding_format_key = "2"
     file_name = "dinov2_vitb14_egg1"
 
-    # Create dataset for training
+    # create dataset for training
     result_excel_path = "/home/gilnetanel/Desktop/results/" + file_name + ".xlsx"
     df = pd.read_excel(result_excel_path, header=None)
     embedding_format = embedding_formats_dict.get(embedding_format_key)
     desired_df = get_values_according2_embedding_format(df, embedding_format)
     training_dataset = desired_df.to_numpy()
+    input_size = training_dataset.shape[0]
 
     # creates model instance and move it to cuda
-    model = NeuralNetwork(embedding_size)
+    model = NeuralNetwork()
     model.cuda()
+
+    # create data loader
+    # training_loader = torch.utils.data.DataLoader(training_dataset, batch_size=4, shuffle=False)
+
+    # loss function
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    # optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+
+    # Initializing in a separate cell so we can easily add more epochs to the same run
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
+    epoch_number = 0
+
+    EPOCHS = 5
+
+    best_vloss = 1_000_000.
+
+    for epoch in range(EPOCHS):
+        print('EPOCH {}:'.format(epoch_number + 1))
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_one_epoch(epoch_number, writer)
+
+        running_vloss = 0.0
+        # Set the model to evaluation mode, disabling dropout and using population
+        # statistics for batch normalization.
+        model.eval()
+
+        # Disable gradient computation and reduce memory consumption.
+        with torch.no_grad():
+            for i, vdata in enumerate(validation_loader):
+                vinputs, vlabels = vdata
+                voutputs = model(vinputs)
+                vloss = loss_fn(voutputs, vlabels)
+                running_vloss += vloss
+
+        avg_vloss = running_vloss / (i + 1)
+        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+        # Log the running loss averaged per batch
+        # for both training and validation
+        writer.add_scalars('Training vs. Validation Loss',
+                           {'Training': avg_loss, 'Validation': avg_vloss},
+                           epoch_number + 1)
+        writer.flush()
+
+        # Track best performance, and save the model's state
+        if avg_vloss < best_vloss:
+            best_vloss = avg_vloss
+            model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+            torch.save(model.state_dict(), model_path)
+
+        epoch_number += 1
+
+
